1e0cb945aaad240480bbd96dc52a3b86dce30296
Delta <hassan911ee@gmail.com>
2022-08-21 02:16:19 +0400

    Created navbar

---
+++ components/navbar.js
@@ -0,0 +1,85 @@
+import { colors } from "../scripts/colors.js"
+
+const navBar = document.createElement("template")
+navBar.innerHTML =
+`
+<style>
+header{
+    width:100vw;
+    height:10vh;
+    background-color: ${colors["pixel-1"]};
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-family:"Poppins", sans-serif;
+}
+
+.brand{
+    margin:20px;
+    cursor:pointer;
+}
+
+.logo{
+    width:2vw;
+    height:5vh;
+}
+
+.t1{
+    font-size: 2rem;
+    font-weight: bold;
+    color:${colors["white"]};
+}
+
+.t2{
+    color:${colors["pixel-5"]};
+}
+
+.items{
+    margin:20px;
+    font-size: 1.3rem;
+    cursor:pointer;
+}
+
+.items a{
+    text-decoration: none;
+    color:${colors["white"]};
+    padding: 10px;
+    transition: 0.2s ease-in-out;
+}
+
+.items a:hover{
+    color:${colors["pixel-5"]};
+}
+
+</style>
+
+
+
+<header>
+<div class = "brand" id="brand">
+<span class="t1">
+Frozen<span class="t2">Pixel</span>
+</span>
+</div>
+
+<div class = "items">
+<a href="games.html">Games</a>
+<a href="team.html">Team</a>
+<a href="">Merch</a> 
+<a href="contact.html">Contact</a>
+</div>
+</header>
+`
+
+class NavBar extends HTMLElement{
+    constructor(){
+        super()
+        this.attachShadow({mode:"open"})
+        this.shadowRoot.appendChild(navBar.content.cloneNode(true))
+        this.shadowRoot.getElementById("brand").addEventListener("click", ()=>{
+            window.open("https://frozenpixel-games.github.io/index.html")
+        })
+    }
+}
+
+window.customElements.define("nav-bar", NavBar)
---
+++ scripts/colors.js
@@ -0,0 +1,12 @@
+export let colors = {
+    "pixel-1":"#03045e",
+    "pixel-2":"#023e8a",
+    "pixel-3":"#0077b6",
+    "pixel-4":"#0096c7",
+    "pixel-5":"#00b4d8",
+    "pixel-6":"#48cae4",
+    "pixel-7":"#90e0ef",
+    "pixel-8":"#ade8f4",
+    "pixel-9":"#caf0f8",
+    "white":"#ffffff"
+}
