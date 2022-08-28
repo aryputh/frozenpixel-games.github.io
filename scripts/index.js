3a1368048df01c217394e60d1a223e654e3c1047
Delta <hassan911ee@gmail.com>
2022-08-28 16:35:11 +0400

    Added itch link + new feature

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,24 @@
-let downloadButton = document.getElementById("download");
+let playStore = document.getElementById("playStoreLink");
 let latestGame = document.getElementById("latestGame");
+let itch = document.getElementById("itchLink");
+let info = document.getElementById("info");
 
-downloadButton.addEventListener("click", () => {
+playStore.addEventListener("click", () => {
   window.open("https://play.google.com/store/apps/dev?id=5025286485091152628");
+});
+
+itch.addEventListener("click", () => {
+  window.open("https://happygamer3194.itch.io/");
+});
+
+latestGame.addEventListener("mouseover", () => {
+  latestGame.style.transform = "translateY(-40px)";
+  info.style.opacity = "100%";
+});
+
+latestGame.addEventListener("mouseleave", () => {
+  latestGame.style.transform = "";
+  info.style.opacity = "0%";
 });
 
 latestGame.addEventListener("click", () => {
