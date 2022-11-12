c566486b72946698194bfaff34e8afb4c317e4de
Souvlaki42 <souvlaki420@gmail.com>
2022-11-13 01:11:45 +0200

    Custom Cursor 1

--- scripts/global.js
+++ scripts/global.js
@@ -1,2 +1,21 @@
-const year = document.querySelector("footer");
-year.innerHTML = `&copy; ${new Date().getFullYear().toString()} FrozenPixel Games`;
+// Footer
+document.querySelector("footer").innerHTML = `&copy; ${new Date().getFullYear().toString()} FrozenPixel Games`;
+
+// Cursor
+const cursor = document.createElement("div");
+cursor.classList.add("cursor");
+
+document.body.appendChild(cursor);
+document.addEventListener("mousemove", (e) => {
+    const mouseLeft = e.pageX + 4;
+    const mouseTop = e.pageY + 4;
+
+    cursor.style.left = `${mouseLeft}px`;
+    cursor.style.top = `${mouseTop}px`;
+});
+document.addEventListener("mousedown", () => {
+    if (!cursor.classList.contains("large")) cursor.classList.add("large");
+});
+document.addEventListener("mouseup", () => {
+    if (cursor.classList.contains("large")) cursor.classList.remove("large");
+})
