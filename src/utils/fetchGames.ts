2141699e2fb106ba552738a3565eaed6ff7fae96
Souvlaki42 <souvlaki420@gmail.com>
2023-03-17 20:24:58 +0200

    CSS not working!

--- src/utils/fetchGames.ts
+++ src/utils/fetchGames.ts
@@ -1,4 +1,4 @@
 export default async function fetchGames() {
-    const response = await fetch(`https://corsproxy.io/?https://itch.io/api/1/${import.meta.env.PUBLIC_ITCH_IO_API_KEY}/my-games`, { method: "GET"});
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/https://itch.io/api/1/${import.meta.env.PUBLIC_ITCH_IO_API_KEY}/my-games`, { method: "GET"});
     return response.json();
 };
